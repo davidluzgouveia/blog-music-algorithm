246cc22da78782c77c0a7dd59063142332297163
David Gouveia <davidluzgouveia@gmail.com>
2013-10-26 21:25:35 +0100

    Initial commit

---
+++ MusicAlgorithm/MusicAlgorithmForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MusicAlgorithm
+{
+    partial class MusicAlgorithmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicAlgorithmForm));
+            this.codeBar = new System.Windows.Forms.TextBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.demoList = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // codeBar
+            // 
+            this.codeBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeBar.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBar.Location = new System.Drawing.Point(50, 12);
+            this.codeBar.Name = "codeBar";
+            this.codeBar.Size = new System.Drawing.Size(500, 22);
+            this.codeBar.TabIndex = 0;
+            // 
+            // playButton
+            // 
+            this.playButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.playButton.Location = new System.Drawing.Point(487, 35);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(63, 26);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.PlayButtonClick);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.stopButton.Location = new System.Drawing.Point(412, 36);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(69, 25);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.StopButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Demo";
+            // 
+            // demoList
+            // 
+            this.demoList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.demoList.FormattingEnabled = true;
+            this.demoList.Location = new System.Drawing.Point(53, 39);
+            this.demoList.Name = "demoList";
+            this.demoList.Size = new System.Drawing.Size(348, 21);
+            this.demoList.TabIndex = 5;
+            this.demoList.SelectedIndexChanged += new System.EventHandler(this.DemoListSelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(102, 78);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(435, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://countercomplex.blogspot.com/2011/10/algorithmic-symphonies-from-one-line-o" +
+    "f.html";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Original concept";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Disclaimer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(15, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(535, 201);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(395, 339);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(155, 13);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "http://www.david-gouveia.com";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(315, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Source code at";
+            // 
+            // MusicAlgorithmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 369);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.demoList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.codeBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MusicAlgorithmForm";
+            this.Text = "Music Algorithm (Uses the XNA Audio API)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox codeBar;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox demoList;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MusicAlgorithm/MusicAlgorithmForm.cs
@@ -0,0 +1,156 @@
+﻿namespace MusicAlgorithm
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Text;
+    using System.Threading;
+    using System.Windows.Forms;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Audio;
+
+    public partial class MusicAlgorithmForm : Form
+    {
+        public MusicAlgorithmForm()
+        {
+            InitializeComponent();
+            PopulateDemoList();
+        }
+
+        private void PopulateDemoList()
+        {
+            demoList.Items.Add(@"");
+            demoList.Items.Add(@"(t>>6|t|t>>(t>>16))*10+((t>>11)&7)");
+            demoList.Items.Add(@"(t*(4|7&t>>13)>>((~t>>11)&1)&128) + ((t)*(t>>11&t>>13)*((~t>>9)&3)&127)");
+            demoList.Items.Add(@"((t*(t>>8|t>>9))&46&t>>8)^(t&t>>13|t>>6)");
+            demoList.Items.Add(@"(t&t%255)-(t*3&t>>13&t>>6)");
+            demoList.Items.Add(@"t*((t>>12|t>>8)&63&t>>4)");
+            demoList.Items.Add(@"t*(t>>11&t>>8&123&t>>3)");
+            demoList.Items.Add(@"t*((t>>9|t>>13)&25&t>>6) ");
+            demoList.Items.Add(@"(t*(t>>5|t>>8))>>(t>>16)");
+            demoList.Items.Add(@"t*5&(t>>7)|t*3&(t*4>>10)");
+            demoList.Items.Add(@"(t|(t>>9|t>>7))*t&(t>>11|t>>9)");
+            demoList.SelectedIndex = 0;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // Create byte buffer for storing and submitting audio samples
+            _buffer = new byte[256*2];
+ 
+            // Create a new DynamicSoundEffectInstance at 8Khz Mono
+            _instance = new DynamicSoundEffectInstance(8000, AudioChannels.Mono);
+            
+            // Creates and starts the audio thread
+            new Thread(AudioThread).Start();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Triggers the audio thread to close
+            _running = false;
+        }
+
+        private void AudioThread()
+        {
+            // Service the XNA Framework once before everything is started
+            FrameworkDispatcher.Update();
+
+            // Start the DynamicSoundEffectInstance
+            _instance.Play();
+
+            // Go into a loop that repeats around 20 times per second (50ms sleep)
+            while (_running)
+            {
+                // Service the XNA Framework once per loop
+                FrameworkDispatcher.Update();
+
+                // Fill and submit buffer
+                while (_instance.PendingBufferCount < 3) 
+                    SubmitBuffer();
+                
+                Thread.Sleep(50);
+            }
+        }
+
+        private void SubmitBuffer()
+        {
+            // Fill the buffer and advance time
+            for (int i = 0; i != _buffer.Length; ++i, ++_time)
+                _buffer[i] = (byte) GetSample();
+
+            // Submit buffer
+            _instance.SubmitBuffer(_buffer);
+        }
+
+        private int GetSample()
+        {
+            // Redirects the call to our compiled code if any
+            return _generatorType != null ? (int) _generatorType.GetMethod("Generate").Invoke(null, new object[] {_time}) : 0;
+        }
+
+        private void SetAlgorithm(string algorithm)
+        {
+            // Reset values
+            _time = 0;
+            _generatorType = null;
+
+            // Ignore if the user wrote nothing
+            if (String.IsNullOrWhiteSpace(algorithm))
+                return;
+
+            // Create code string
+            StringBuilder codeBuilder = new StringBuilder();
+            codeBuilder.AppendLine("namespace MusicAlgorithm {");
+            codeBuilder.AppendLine("    public static class AudioGenerator {");
+            codeBuilder.AppendLine("        public static int Generate(int t) { return " + algorithm + "; }");
+            codeBuilder.AppendLine("    }");
+            codeBuilder.AppendLine("}");
+            string code = codeBuilder.ToString();
+
+            // Compile code string in memory
+            CodeDomProvider codeDomProvider = CodeDomProvider.CreateProvider("C#");
+            CompilerParameters compileParams = new CompilerParameters { GenerateExecutable = false, GenerateInMemory = true };
+            CompilerResults compilerResults = codeDomProvider.CompileAssemblyFromSource(compileParams, new[] {code});
+            
+            // On error display message and exit
+            if (compilerResults.Errors.HasErrors)
+            {
+                MessageBox.Show(GetErrorMessage(compilerResults));
+                return;
+            }
+
+            // Otherwise store a Type reference to the assembly we compiled
+            // Using reflection we can call the Generate method from this Type
+            _generatorType = compilerResults.CompiledAssembly.GetType("MusicAlgorithm.AudioGenerator");
+        }
+
+        private static string GetErrorMessage(CompilerResults compilerResults)
+        {
+            StringBuilder errorBuilder = new StringBuilder();
+            foreach (CompilerError compilerError in compilerResults.Errors)
+                errorBuilder.AppendLine(compilerError.ErrorText);
+            return errorBuilder.ToString();
+        }
+
+        private void PlayButtonClick(object sender, EventArgs e)
+        {
+            SetAlgorithm(codeBar.Text);
+        }
+
+        private void StopButtonClick(object sender, EventArgs e)
+        {
+            SetAlgorithm("0");
+        }
+
+        private void DemoListSelectedIndexChanged(object sender, EventArgs e)
+        {
+            codeBar.Text = (string)demoList.SelectedItem;
+        }
+
+        private DynamicSoundEffectInstance _instance;
+        private byte[] _buffer;
+        private int _time;
+        private Type _generatorType;
+        private bool _running = true;
+    }
+}
